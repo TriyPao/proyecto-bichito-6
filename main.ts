c8b9d5042a877d3ec442af8023e8f8f957714624
TriyPao <107255030+TriyPao@users.noreply.github.com>
2022-06-14 09:27:11 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+basic.showString("Hola!")
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) < -20) {
+        basic.showString("<")
+    }
+    if (input.acceleration(Dimension.X) > 20) {
+        basic.showString(">")
+    }
+    if (input.acceleration(Dimension.Y) > 20) {
+        basic.showString("v")
+    }
+    if (input.acceleration(Dimension.Y) < -20) {
+        basic.showString("^")
+    }
+})
